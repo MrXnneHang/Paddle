310d3404c13c4fe933ad5e67f20bf49b94ef58a2
MrXnneHang <XnneHang@gmail.com>
2024-12-08 22:58:08 +0800

    use printf to void cuda not support

--- paddle/phi/kernels/funcs/compare_functors.h
+++ paddle/phi/kernels/funcs/compare_functors.h
@@ -14,7 +14,6 @@
 
 #pragma once
 #include <math.h>
-#include <iostream>
 #include "paddle/phi/common/complex.h"
 namespace phi {
 namespace funcs {
@@ -36,7 +35,7 @@ COMPARE_FUNCTOR(GreaterEqualFunctor, >=)
 template <typename InT, typename OutT = bool>
 struct EqualFunctor {
   HOSTDEVICE OutT operator()(const InT a, const InT b) const {
-    std::cout << "NotEqual泛化模板。\n";
+    printf("NotEqual泛化模板。\n");
     if (std::is_floating_point<InT>::value) {
       if (isinf(static_cast<float>(a)) || isinf(static_cast<float>(b)))
         return static_cast<OutT>(a == b);
@@ -53,18 +52,18 @@ template <typename T>
 struct EqualFunctor<phi::dtype::complex<T>> {
   HOSTDEVICE bool operator()(const phi::dtype::complex<T> a,
                              const phi::dtype::complex<T> b) const {
-    std::cout << "Equal偏特化模板\n";
+    printf("Equal偏特化模板\n");
     if (isnan(static_cast<T>(a.real)) || isnan(static_cast<T>(a.imag)) ||
         isnan(static_cast<T>(b.real)) || isnan(static_cast<T>(b.imag))) {
-      std::cout << "Equal偏特化:存在NAN\n";
+      printf("Equal偏特化:存在NAN\n");
       return static_cast<bool>(false);
     }
     if (isinf(static_cast<T>(a.real)) || isinf(static_cast<T>(a.imag)) ||
         isinf(static_cast<T>(b.real)) || isinf(static_cast<T>(b.imag))) {
-      std::cout << "Equal偏特化:存在INF\n";
+      printf("Equal偏特化:存在INF\n");
       return static_cast<bool>(a.real == b.real && a.imag == b.imag);
     }
-    std::cout << "Equal偏特化:正常数.\n";
+    printf("Equal偏特化:正常数.\n");
     return static_cast<bool>(fabs(static_cast<double>(a.real - b.real)) <
                                  1e-8 &&
                              fabs(static_cast<double>(a.imag - b.imag)) < 1e-8);
@@ -74,7 +73,7 @@ struct EqualFunctor<phi::dtype::complex<T>> {
 template <typename InT, typename OutT = bool>
 struct NotEqualFunctor {
   HOSTDEVICE bool operator()(const InT a, const InT b) const {
-    std::cout << "NotEqual泛化模板。\n";
+    printf("NotEqual泛化模板。\n");
     return !EqualFunctor<InT, OutT>()(a, b);
   }
 };
@@ -83,7 +82,7 @@ template <typename T>
 struct NotEqualFunctor<phi::dtype::complex<T>> {
   HOSTDEVICE bool operator()(const phi::dtype::complex<T> a,
                              const phi::dtype::complex<T> b) const {
-    std::cout << "NotEqual偏特化模板。\n";
+    printf("NotEqual偏特化模板。\n");
     return !EqualFunctor<phi::dtype::complex<T>>()(a, b);
   }
 };
