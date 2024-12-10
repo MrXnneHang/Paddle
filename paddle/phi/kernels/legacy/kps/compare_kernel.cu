1569b7f068654fd0e892333dffc62a412335bef7
MrXnneHang <XnneHang@gmail.com>
2024-12-10 14:46:14 +0800

    同时注册所有op.增加compare op 的printf.

--- paddle/phi/kernels/legacy/kps/compare_kernel.cu
+++ paddle/phi/kernels/legacy/kps/compare_kernel.cu
@@ -166,36 +166,35 @@ PD_REGISTER_KERNEL(less_than_raw,
                      int64_t,                             \
                      float,                               \
                      double,                              \
+                     phi::dtype::complex<float>,          \
+                     phi::dtype::complex<double>,         \
                      phi::dtype::float16,                 \
                      phi::dtype::bfloat16) {              \
     kernel->OutputAt(0).SetDataType(phi::DataType::BOOL); \
   }
 
-#define PD_REGISTER_COMPLEX_COMPARE_RAW_KERNEL(name, func) \
-  PD_REGISTER_KERNEL(name##_raw,                           \
-                     KPS,                                  \
-                     ALL_LAYOUT,                           \
-                     phi::func##RawKernel,                 \
-                     bool,                                 \
-                     uint8_t,                              \
-                     int16_t,                              \
-                     int,                                  \
-                     int8_t,                               \
-                     int64_t,                              \
-                     phi::dtype::complex<float>,           \
-                     phi::dtype::complex<double>,          \
-                     float,                                \
-                     double,                               \
-                     phi::dtype::float16,                  \
-                     phi::dtype::bfloat16) {               \
-    kernel->OutputAt(0).SetDataType(phi::DataType::BOOL);  \
-  }
+// #define PD_REGISTER_COMPLEX_COMPARE_RAW_KERNEL(name, func) \
+//   PD_REGISTER_KERNEL(name##_raw,                           \
+//                      KPS,                                  \
+//                      ALL_LAYOUT,                           \
+//                      phi::func##RawKernel,                 \
+//                      bool,                                 \
+//                      uint8_t,                              \
+//                      int16_t,                              \
+//                      int,                                  \
+//                      int8_t,                               \
+//                      int64_t,                              \
+//                      phi::dtype::complex<float>,           \
+//                      phi::dtype::complex<double>,          \
+//                      float,                                \
+//                      double,                               \
+//                      phi::dtype::float16,                  \
+//                      phi::dtype::bfloat16) {               \
+//     kernel->OutputAt(0).SetDataType(phi::DataType::BOOL);  \
+//   }
 
 PD_REGISTER_COMPARE_RAW_KERNEL(less_equal, LessEqual)
 PD_REGISTER_COMPARE_RAW_KERNEL(greater_than, GreaterThan)
 PD_REGISTER_COMPARE_RAW_KERNEL(greater_equal, GreaterEqual)
-
-PD_REGISTER_COMPLEX_COMPARE_RAW_KERNEL(equal, Equal)
-PD_REGISTER_COMPLEX_COMPARE_RAW_KERNEL(not_equal, NotEqual)
-
-#endif
+PD_REGISTER_COMPARE_RAW_KERNEL(equal, Equal)
+PD_REGISTER_COMPARE_RAW_KERNEL(not_equal, NotEqual)
